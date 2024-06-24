828de2fcb181178f7712372993872e98a5ed11e6
srh0820 <srh96820@gmail.com>
2024-06-24 17:57:17 +0900

    헤더 스크롤 이벤트

--- ajax/js/ui.js
+++ ajax/js/ui.js
@@ -2,9 +2,9 @@ $(function(){
     $(window).on('scroll', function(){  // this 개념 조심하기
         let scrollps = $(this).scrollTop();
         if( scrollps > 80 ){ // 스크롤이 아래로
-            $('#hd').addClass("bg-dark border-bottom border-white").removeClass("bg-white")
+            $('#hd').addClass("scroll")
         }else{
-            $('#hd').removeClass("bg-dark border-bottom border-white").addClass("bg-white")
+            $('#hd').removeClass("scroll")
         }
     })
 })
